56dc0c6d86c882c179d78168ebe85dccdb889d97
aravindh2000 <78861911+aravindh2000@users.noreply.github.com>
2023-03-17 20:30:11 +0530

    first cmt

--- demo/src/main/java/com/example/demo/Ctrl.java
+++ demo/src/main/java/com/example/demo/Ctrl.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import ModelPack.UserRequest;
+import data.CollectedUserData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ public class Ctrl {
     @GetMapping("/api")
     public String getData(@RequestBody UserRequest userRequest)
     {
+        CollectedUserData cud=new CollectedUserData();
+        cud.setNameOfUser(userRequest.getUserName());
         return "siva";
     }
 }
---
+++ demo/src/main/java/data/CollectedUserData.java
@@ -0,0 +1,15 @@
+package data;
+
+public class CollectedUserData {
+
+
+    String nameOfUser;
+
+    public CollectedUserData() {
+
+    }
+
+    public void setNameOfUser(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+}
